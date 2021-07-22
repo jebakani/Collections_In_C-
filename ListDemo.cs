8d9c94771054e01dce2b8cae6886e224b1b8aeef
Jebakani <vjishwarya@gmail.com>
2021-07-22 19:05:25 +0530

    [JEBAKANI] Executing the List functions

---
+++ ListDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class ListDemo
+    {
+        //method to display the list functions
+        public static void ExecuteList()
+        {
+            Console.WriteLine("Creating list");
+            List<string> list = new List<string>();
+            list.Add("abc");
+            list.Add("tvs");
+            list.Add("xyz");
+            list.Add("BridgeLabz");
+            Console.WriteLine("Displaying the list items");
+            DisplayList(list);
+            Console.WriteLine("\nRemove the element");
+            list.Remove("xyz");
+            Console.WriteLine("After removal");
+            DisplayList(list);
+            Console.WriteLine("\nElement at 2nd position is {0}", list[2]);
+
+
+        }
+        //Method to display the list
+        public static void DisplayList(List<string> list)
+        {
+            foreach (string l in list)
+            {
+                Console.WriteLine(l);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace Collections
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Collection Library");
+            Console.WriteLine("1.List");
+            Console.WriteLine("Enter you choice");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    ListDemo.ExecuteList();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
